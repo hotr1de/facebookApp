17b78cb970a7131d55cb6f4ab0030c2548b7eaff
Cooper Reid <creid.student@manhattan.edu>
2010-03-28 17:22:50 -0400

    mod index.php

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
-// Copyright 2007 Facebook Corp.  All Rights Reserved. 
-// 
-// Application: Test Guessing Game
-// File: 'index.php' 
-//require_once 'facebook.php';
+// Application: tag cloud 
 require_once 'facebookAPI/facebook-platform/php/facebook.php';
 $appapikey = '59f818f601c19ba21c6124f6631b23cf';
 $appsecret = 'ac08c579883901c53ac76d3296f4daaa';
@@ -12,14 +8,18 @@ $user_id = $facebook->require_login();
 $currentUser = $facebook->api_client->users_getLoggedInUser();
 $userFullName = $facebook->api_client->users_getInfo($currentUser,'first_name, last_name');
 $userFullName = $userFullName[0]['first_name']." ".$userFullName[0]['last_name'];
+
 // Greet the currently logged-in user!
 echo "<p>Hello, $userFullName</p>";
-//use 32104238 for a mock user
+
+//use 32104238 as user 
 $info = $facebook->api_client->users_getInfo($currentUser,'music,interests');
 $music = $info[0]['music'];
 $interests = $info[0]['interests'];
 $info = $music.",".$interests;
 $info = preg_split("/,/",$info);
+
+//draw cloud
 foreach($info as $tag) {
   ?>
   <div style="position:absolute; top:<?php echo rand(30,475); ?>px; left:<?php echo rand(0,500); ?>px">
