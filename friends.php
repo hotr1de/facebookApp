081512c24aa2fcd95396bebc0a5fe16b534287cf
Cooper Reid <creid.student@manhattan.edu>
2010-04-20 10:56:32 -0400

    fixed IE bug and removed unneeded code

--- friends.php
+++ friends.php
@@ -1,8 +1,6 @@
 <?php
 // Application: tag cloud 
-
 error_reporting(E_ALL);
-
 ?>
 <head>
 <link rel='stylesheet' href='includes/style.css' media='screen' type='text/css'>
@@ -11,17 +9,18 @@ error_reporting(E_ALL);
 </head>
 <body>
 <?php
-//require_once 'includes/binTree.php';
+//include the binTree cloud generator and faceboko API
 require_once 'includes/binTree.php';
 require_once 'facebookAPI/facebook-platform/php/facebook.php';
 $appapikey = '59f818f601c19ba21c6124f6631b23cf';
 $appsecret = 'ac08c579883901c53ac76d3296f4daaa';
 $facebook = new Facebook($appapikey, $appsecret);
+//require the user to be logged in
 $user_id = $facebook->require_login();
-$currentUser = $facebook->api_client->users_getLoggedInUser();
-$userFullName = $facebook->api_client->users_getInfo($currentUser,'first_name, last_name');
-$userFullName = $userFullName[0]['first_name']." ".$userFullName[0]['last_name'];
-
+//$currentUser = $facebook->api_client->users_getLoggedInUser();
+//echo $user_id;
+//$userFullName = $facebook->api_client->users_getInfo($currentUser,'first_name, last_name');
+//$userFullName = $userFullName[0]['first_name']." ".$userFullName[0]['last_name'];
 
 //retrieve interest to compare with
 $interest = $_GET['tag'];
@@ -29,13 +28,17 @@ $interest = str_replace("_"," ",$interest);
 $interest = trim($interest);
 
 //use FQL to do to compare interests
-$query = "SELECT uid, name, pic, hometown_location, activities, interests, music, tv, movies, books, profile_url FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = '$currentUser')";
+$query = "SELECT uid, name, pic, hometown_location, activities, interests, music, tv, movies, books, profile_url FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = '$user_id')";
 
+//perform query
 $result=$facebook->api_client->fql_query($query);
+
+//arrays hold users that share a tag - their name, uid, and pic url
 $hasInCommonNm = array();
 $hasInCommonID = array();
 $pics = array();
-$name = "";
+
+//add push user data to each array if they share a given tag
 for($i = 0; $i < count($result); $i++) {
    if(stristr($result[$i]['music'], $interest) || 
       stristr($result[$i]['activities'], $interest) || 
@@ -49,16 +52,21 @@ for($i = 0; $i < count($result); $i++) {
      array_push($pics, $result[$i]['pic']);
    }
 }
+
+//set post variables in order to use user data in binTree.php
 $_POST['users'] = $hasInCommonID;
 $_POST['pics'] = $pics;
+
+//build cloud displaying usernames
 $friendTree = buildTree($hasInCommonNm, "user");
 ?>
 
-
 Friends who like <i><?php echo $interest; ?></i>
+
 <div style="width:500px;">
 <?php $friendTree->prin(1,0); ?>
 </div>
+
 <?php
 ?>
 
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 // Application: tag cloud 
-
+//header('P3P: CP="CAO PSA OUR"');
+header('P3P:CP="IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT"');
 error_reporting(E_ALL);
 
 require_once 'includes/binTree.php';
@@ -9,8 +10,8 @@ $appapikey = '59f818f601c19ba21c6124f6631b23cf';
 $appsecret = 'ac08c579883901c53ac76d3296f4daaa';
 $facebook = new Facebook($appapikey, $appsecret);
 $user_id = $facebook->require_login();
-$currentUser = $facebook->api_client->users_getLoggedInUser();
-$userFullName = $facebook->api_client->users_getInfo($currentUser,'name');
+//$currentUser = $facebook->api_client->users_getLoggedInUser();
+$userFullName = $facebook->api_client->users_getInfo($user_id,'name');
 $userFullName = $userFullName[0]['name'];
 
 ?>
@@ -20,16 +21,17 @@ $userFullName = $userFullName[0]['name'];
 <body>
 
 <?php
-if(isset($_GET['uid'])) $currentUser = $_GET['uid'];
+if(isset($_GET['uid'])) $user_id = $_GET['uid'];
 if(isset($_GET['name'])) $userFullName = $_GET['name'];
 
+//unset 'shared' users data if it exists
 if(isset($_POST['users'])) unset($_POST['users']);
 if(isset($_POST['pics'])) unset($_POST['pics']);
 
 // state whose info it is
 echo "<p>$userFullName likes :</p>";
 
-$info = $facebook->api_client->users_getInfo($currentUser,'music,interests,movies, activities, tv, books');
+$info = $facebook->api_client->users_getInfo($user_id,'music,interests,movies, activities, tv, books');
 
 $music = $info[0]['music'];
 $interests = $info[0]['interests'];
@@ -39,7 +41,7 @@ $tv = $info[0]['tv'];
 $books = $info[0]['books'];
 $info = $music.",".$interests.",".$movies.",".$activities.",".$tv.",".$books;
 
-//splits the info string delimited by commas and periods
+//splits the info string delimited by , ; / .
 //and puts each element in the new info array
 $info = preg_split("/[,.\/\;]+/",$info);
 $infoTree = buildtree($info, "info");
